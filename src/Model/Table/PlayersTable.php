8b5bf11969fc23c44eb1b5382939802be1d90027
hirotaku <hirosawa@fusic.co.jp>
2017-05-26 17:08:56 +0900

    プレイヤーテーブルbake

---
+++ src/Model/Table/PlayersTable.php
@@ -0,0 +1,99 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Players Model
+ *
+ * @property \Cake\ORM\Association\BelongsTo $Teams
+ *
+ * @method \App\Model\Entity\Player get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Player newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Player[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Player|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Player patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Player[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Player findOrCreate($search, callable $callback = null, $options = [])
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ */
+class PlayersTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('players');
+        $this->setDisplayField('name');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+        $this->belongsTo('Teams', [
+            'foreignKey' => 'team_id'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->allowEmpty('name');
+
+        $validator
+            ->allowEmpty('position');
+
+        $validator
+            ->allowEmpty('number');
+
+        $validator
+            ->allowEmpty('birthday');
+
+        $validator
+            ->allowEmpty('nationality');
+
+        $validator
+            ->boolean('deleted')
+            ->allowEmpty('deleted');
+
+        $validator
+            ->dateTime('deleted_date')
+            ->allowEmpty('deleted_date');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['team_id'], 'Teams'));
+
+        return $rules;
+    }
+}
---
+++ tests/Fixture/PlayersFixture.php
@@ -0,0 +1,57 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * PlayersFixture
+ *
+ */
+class PlayersFixture extends TestFixture
+{
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 10, 'autoIncrement' => true, 'default' => null, 'null' => false, 'comment' => null, 'precision' => null, 'unsigned' => null],
+        'team_id' => ['type' => 'integer', 'length' => 10, 'default' => null, 'null' => true, 'comment' => null, 'precision' => null, 'unsigned' => null, 'autoIncrement' => null],
+        'name' => ['type' => 'string', 'length' => null, 'default' => null, 'null' => true, 'collate' => null, 'comment' => null, 'precision' => null, 'fixed' => null],
+        'position' => ['type' => 'string', 'length' => null, 'default' => null, 'null' => true, 'collate' => null, 'comment' => null, 'precision' => null, 'fixed' => null],
+        'number' => ['type' => 'string', 'length' => null, 'default' => null, 'null' => true, 'collate' => null, 'comment' => null, 'precision' => null, 'fixed' => null],
+        'birthday' => ['type' => 'string', 'length' => null, 'default' => null, 'null' => true, 'collate' => null, 'comment' => null, 'precision' => null, 'fixed' => null],
+        'nationality' => ['type' => 'string', 'length' => null, 'default' => null, 'null' => true, 'collate' => null, 'comment' => null, 'precision' => null, 'fixed' => null],
+        'deleted' => ['type' => 'boolean', 'length' => null, 'default' => 0, 'null' => true, 'comment' => null, 'precision' => null],
+        'deleted_date' => ['type' => 'timestamp', 'length' => null, 'default' => null, 'null' => true, 'comment' => null, 'precision' => null],
+        'created' => ['type' => 'timestamp', 'length' => null, 'default' => null, 'null' => true, 'comment' => null, 'precision' => null],
+        'modified' => ['type' => 'timestamp', 'length' => null, 'default' => null, 'null' => true, 'comment' => null, 'precision' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'id' => 1,
+            'team_id' => 1,
+            'name' => 'Lorem ipsum dolor sit amet',
+            'position' => 'Lorem ipsum dolor sit amet',
+            'number' => 'Lorem ipsum dolor sit amet',
+            'birthday' => 'Lorem ipsum dolor sit amet',
+            'nationality' => 'Lorem ipsum dolor sit amet',
+            'deleted' => 1,
+            'deleted_date' => 1495786102,
+            'created' => 1495786102,
+            'modified' => 1495786102
+        ],
+    ];
+}
